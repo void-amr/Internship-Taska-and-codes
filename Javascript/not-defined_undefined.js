8f65531d8abf024bc0b2849b7169f74c91c8234e
aryan <aryanranjane9@gmail.com>
2024-06-16 21:51:25 +0530

    21:51pm 16/6/24

---
+++ Javascript/not-defined_undefined.js
@@ -0,0 +1,17 @@
+/* 
+ 	@Author -> AryanRanjane(ranjanearyan14@gmail.com)
+	@Brief -> difference defined and undefined 
+	@Date -> 16/6/24 
+ */
+
+/* 
+ *	Undefined
+ *	Notdefined
+ *
+ *	undefined --> variable is declared but is not initialized 
+ *	not-defined --> variable is not defined.
+ */ 
+
+var a;
+console.log(a);
+console.log(lol);
