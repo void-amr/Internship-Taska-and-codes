2bcda14ca9d5ddddfed673724b116b9bbc7f1986
void <aryanranjane9@gmail.com>
2024-06-23 00:24:06 +0530

    22/6/24 00:25am

--- Javascript/Dom/color_change.js
+++ Javascript/Dom/color_change.js
@@ -4,23 +4,4 @@
  * @Date -> 21/6/24
  */
 
-const MAIN = document.getElementById('main');
-const BULB = document.getElementById('bulb');
-var isTrue = false; 
-
-BULB.addEventListener("click",function(){
-        
-    BULB.style.backgroundColor  = "white";
-    MAIN.style.backgroundColor  ="black";
-    if(isTrue) {
-        change_Color(); 
-    }
-});
-
-function change_Color() {
-        
-    BULB.addEventListener("click",()=>{
-        BULB.style.backgroundColor  = "yellow";
-        MAIN.style.backgroundColor  ="white";
-    });
-}
+i 
