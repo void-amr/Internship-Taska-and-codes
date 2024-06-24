be0758e8a27918253a18ce79d49339a522bde773
void <aryanranjane9@gmail.com>
2024-06-24 14:43:23 +0530

    14:15pm 24/6/24

--- Javascript/Dom/color_change.js
+++
@@ -1,6 +0,0 @@
-/**
- * @Author -> AryanRanjane(ranjanaryan14@gmail.com)
- * @Brief -> Demonstration of DOM by making a color_changing circle
- * @Date -> 21/6/24
- */
-
--- Javascript/fundamentals/scope.js
+++
@@ -1,4 +0,0 @@
-/*
- *  @Author -> AryanRanjane(ranjanearyan14@gmail.com)
- *  @Date -> 19/6/24
- */
