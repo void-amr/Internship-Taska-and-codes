33baac23954f28cbc34c269d689b4824f7c5397c
void <aryanranjane9@gmail.com>
2024-06-24 14:06:19 +0530

    14:06pm 24/6/24

--- Amazon-clone/src/scripts/index.js
+++ Amazon-clone/src/scripts/index.js
@@ -1,3 +1,9 @@
+/**
+ * @Author -> AryanRanjane(ranjanearyan14@gmail.com)
+ * @Brief -> Js file for the client side logic related to index.html
+ * @Date -> 24/6/24
+ */
 
+const ELECTRONICS = document.getElementById('xyz')
 
     
